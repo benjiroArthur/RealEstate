5d750448529c8c5625024ef4902d32569460bf45
BENJIRO <majorcaios@gmail.com>
2018-09-21 20:43:27 +0000

    third real estate project

---
+++ app/Images.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class images extends Model
+{
+    //
+    public function property()
+    {
+        return $this->belongsTo('App\Properties');
+    }
+}
