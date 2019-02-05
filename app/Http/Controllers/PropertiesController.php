53924f52892682f5b9e668453e6ac829068562ad
BENJIRO <majorcaios@gmail.com>
2019-02-05 12:46:23 +0000

    third real estate project

--- app/Http/Controllers/PropertiesController.php
+++ app/Http/Controllers/PropertiesController.php
@@ -4,6 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Properties;
+use App\Images;
+use App\User;
+use Illuminate\Support\Facades\Session;
+use Image;
+use App\NotifyMe;
+
 
 class PropertiesController extends Controller
 {
@@ -19,8 +25,9 @@ class PropertiesController extends Controller
      */
     public function index()
     {
-        $properties = Properties::orderBy('created_at', 'desc')->paginate(12);
-        return view('properties.index')->with('properties', $properties);
+//        $properties = Properties::where('available', '=', '0')->orderBy('created_at', 'desc')->paginate(12);
+//        return view('properties.index')->with('properties', $properties);
+        //check pagesController@index
     }
 
     /**
@@ -30,6 +37,8 @@ class PropertiesController extends Controller
      */
     public function create()
     {
+//        $trac_list = array('Lease','Sale','Rent');
+
         return view('properties.create');
     }
 
@@ -42,12 +51,90 @@ class PropertiesController extends Controller
     public function store(Request $request)
     {
 
-        //Validate post fields to ensure none empty fields
+//        Validate post fields to ensure none empty fields
         $this->validate($request,[
-            'title' => 'required',
-            'body' => 'required',
-            'cover_image' => 'image|nullable|max:1999'
+            'name' => 'required',
+//           'images' => 'image|nullable|max:1999',
+            'price' => 'required',
+            'address' => 'required',
+            'access' => 'required',
+            'floor_space' => 'required',
+            'utility' => 'required',
+            'description' => 'required',
+            'transaction_type' => 'required',
+
+
         ]);
+
+//Handle file upload
+        if($request->hasFile('images'))
+        {
+            $fileNameCombine = array();
+            foreach ($request->images as $image)
+            {
+
+                $fileNameWithExt = $image->getClientOriginalName();
+
+                //Get just filename
+                $fileName = pathinfo($fileNameWithExt, PATHINFO_FILENAME);
+
+                //Get just extension
+                $extension = $image->getClientOriginalExtension();
+
+                //Filename to store
+                $fileNameToJoin = $fileName.'_'.time().'.'.$extension;
+
+                //resize image
+
+                //upload file
+
+                $path = $image->storeAs('public/images/properties', $fileNameToJoin);
+                 array_push($fileNameCombine, $fileNameToJoin);
+                 $imagePath = public_path('/storage/images/properties/'.$fileNameToJoin);
+                $img = Image::make($imagePath)->resize(450,300)->save($imagePath);
+
+                }
+
+
+
+//            foreach ($fileNameCombine as $test){
+//                echo $test.'<br>';
+//
+//            }
+
+            $fileNameToStore = implode(',', $fileNameCombine);
+//            echo $fileNameToStore;
+//            dd($fileNameToStore);
+        }
+        else
+        {
+            $fileNameToStore = 'noimage.jpg';
+        }
+
+        $properties = new Properties;
+
+
+        $properties->name = $request->input('name');
+        $properties->images = $fileNameToStore;
+        $properties->price = $request->input('price');
+        $properties->address = $request->input('address');
+        $properties->access = $request->input('access');
+        $properties->floor_space = $request->input('floor_space');
+        $properties->utility = $request->input('utility');
+        $properties->description = $request->input('description');
+        $properties->transaction_type = $request->input('transaction_type');
+        $properties->property_type = $request->input('property_type');
+        $properties->structure_type = $request->input('structure_type');
+        $properties->user_id = auth()->user()->id;
+        $properties->available = 1;
+        $properties->class = 0;
+        $properties->save();
+
+//        $notifyme = NotifyMe::all('email');
+
+        return redirect('/dashboard')->with('success', 'Property Added Successfully');
+
+
     }
 
     /**
@@ -59,7 +146,27 @@ class PropertiesController extends Controller
 
     public function show($id)
     {
-        //
+
+
+        //show details of single property
+        $property = Properties::find($id);
+        $image = explode(',', $property->images);
+        $imageCount=count($image);
+        $imageIndex = 0;
+        $i= 0;
+
+        //get user name for the selected post
+        $user_id = $property->user_id;
+        $single_user = User::find($user_id);
+        $name = $single_user->name;
+        $phone = $single_user->phone_number;
+        return view('properties.show')->with('property', $property)
+                                           ->with('imageCount', $imageCount)
+                                           ->with('imageIndex', $imageIndex)
+                                           ->with('i', $i)
+                                           ->with('image', $image)
+                                           ->with('name',$name)
+                                           ->with('phone',$phone);
     }
 
     /**
@@ -70,7 +177,17 @@ class PropertiesController extends Controller
      */
     public function edit($id)
     {
-        //
+
+        //Edit Property
+        $properties = Properties::find($id);
+
+        //check for correct user
+        if(auth()->user()->id !== $properties->user_id)
+        {
+            return redirect('properties.index')->with('error', 'Unauthorised Access');
+        }
+
+        return view('properties.edit')->with('properties', $properties);
     }
 
     /**
@@ -83,6 +200,98 @@ class PropertiesController extends Controller
     public function update(Request $request, $id)
     {
         //
+        //Validate post fields to ensure none empty fields
+        $this->validate($request,[
+            'name' => 'required',
+//           'images' => 'image|nullable|max:1999',
+            'price' => 'required',
+            'address' => 'required',
+            'access' => 'required',
+            'floor_space' => 'required',
+            'utility' => 'required',
+            'description' => 'required',
+            'transaction_type' => 'required',
+
+
+        ]);
+
+//Handle file upload
+        if($request->hasFile('images'))
+        {
+            $fileNameCombine = array();
+            foreach ($request->images as $image)
+            {
+
+                $fileNameWithExt = $image->getClientOriginalName();
+
+                //Get just filename
+                $fileName = pathinfo($fileNameWithExt, PATHINFO_FILENAME);
+
+                //Get just extension
+                $extension = $image->getClientOriginalExtension();
+
+                //Filename to store
+                $fileNameToJoin = $fileName.'_'.time().'.'.$extension;
+
+                //resize image
+
+                //upload file
+
+                $path = $image->storeAs('public/images/properties', $fileNameToJoin);
+                array_push($fileNameCombine, $fileNameToJoin);
+                $imagePath = public_path('/storage/images/properties/'.$fileNameToJoin);
+                $img = Image::make($imagePath)->resize(450,300)->save($imagePath);
+
+            }
+
+
+
+//            foreach ($fileNameCombine as $test){
+//                echo $test.'<br>';
+//
+//            }
+
+            $fileNameToStore = implode(',', $fileNameCombine);
+
+
+        }
+//        else
+//        {
+//            $fileNameToStore = 'noimage.jpg';
+//        }
+
+        $properties = Properties::find($id);
+
+
+
+        $properties->name = $request->input('name');
+        if($request->hasFile('images'))
+        {
+            $properties->images = $fileNameToStore;
+        }
+        $properties->price = $request->input('price');
+        $properties->address = $request->input('address');
+        $properties->access = $request->input('access');
+        $properties->floor_space = $request->input('floor_space');
+        $properties->utility = $request->input('utility');
+        $properties->description = $request->input('description');
+        $properties->transaction_type = $request->input('transaction_type');
+        $properties->property_type = $request->input('property_type');
+        $properties->structure_type = $request->input('structure_type');
+        $properties->user_id = auth()->user()->id;
+
+        if($request->input('available') == 'Yes')
+        {
+            $properties->available = 1;
+        }
+        else
+        {
+            $properties->available = 0;
+        }
+        $properties->save();
+        return redirect('/dashboard')->with('success', 'Property Updated Successfully.');
+
+
     }
 
     /**
@@ -94,5 +303,10 @@ class PropertiesController extends Controller
     public function destroy($id)
     {
         //
+
     }
+
+
+
+
 }
